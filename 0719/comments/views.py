59372430148435953d9864484a4784d764657553
wheymain <wmliu2014@gmail.com>
2017-07-19 22:35:41 +0800

    Update views.py

--- 0719/comments/views.py
+++ 0719/comments/views.py
@@ -19,4 +19,5 @@ def post_comment(request, post_pk):
 			comment_list = post.comment_set.all()
 			context = {'post': post, 'form': form, 'comment_list': comment_list}
 			return render(request, 'blog/detail.html', context=context)
-	return redirect(post)
+	else:
+		return redirect(post)
